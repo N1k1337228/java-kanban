3501c896ac18bf6489e7f6de7f6f12e62df0d7d2
n1k228 <nikitabutov694@gmail>
2025-07-15 14:31:43 +0300

    Первая версия девятого проекта, исправил стиль кода в TaskManager

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -5,37 +5,50 @@ import exceptons.NotFoundException;
 import taskclasses.Epic;
 import taskclasses.SubTask;
 import taskclasses.Task;
+
 import java.util.ArrayList;
 
 public interface TaskManager {
     // Создание задач
-    void createTask(Task task) ;
-    void createEpic(Epic epic) ;
-    void createSubTask(SubTask subTask) ;
+    void createTask(Task task);
+
+    void createEpic(Epic epic);
+
+    void createSubTask(SubTask subTask);
 
     // Обновление задач
     void updateTask(Task task) throws NotFoundException;
+
     void updateEpic(Epic epic) throws NotFoundException;
+
     void updateSabTask(SubTask subTask) throws NotFoundException;
 
     // Получение списков задач
     ArrayList<Task> getTaskList();
+
     ArrayList<Epic> getEpicList();
+
     ArrayList<SubTask> getSubTaskList();
 
     // Удаление всех задач
     void removeTask();
+
     void removeEpics();
+
     void removeSubTask();
 
     // Получение задач по ID
     Task getTask(int id) throws NotFoundException;
+
     Epic getEpic(int id) throws NotFoundException;
+
     SubTask getSubTask(int id) throws NotFoundException;
 
     // Удаление задач по ID
     void removeTaskOnId(int id) throws NotFoundException, ManagerSaveException;
+
     void removeEpicOnId(int id) throws NotFoundException, ManagerSaveException;
+
     void removeSubTaskOnId(int id) throws NotFoundException, ManagerSaveException;
 
     // Получение подзадач эпика
